897aa968debeaeca05bdb79907de2d08b365d1e1
Abhijith V <84576462+abhijithvinodnair@users.noreply.github.com>
2024-10-04 22:39:06 +0530

    Update Snake_Game.py

--- Turtle/Snake Game/Snake_Game.py
+++ Turtle/Snake Game/Snake_Game.py
@@ -27,14 +27,17 @@ while game_is_on:
         food.refresh()
         snake.extend()
         scoreboard.increase_score()
-    for segment in snake.snakes[1:]:
-        if snake.head.distance(segment)< 10:
-            game_is_on = False
-            scoreboard.game_over()
-#Detect collision with wall
+        #Detect collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
+#Detect if collided with wall
+    for segment in snake.snakes[1:]:
+        if segment ==snake.head:
+            pass
+        elif snake.head.distance(segment)< 10:
+            scoreboard.reset() 
+            snake.reset()
 
 
 screen.exitonclick()
